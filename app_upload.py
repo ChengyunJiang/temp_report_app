4a5cbd2973304ade52e5a911b42efb49ead1874b
ChengyunJiang <143630901+ChengyunJiang@users.noreply.github.com>
2025-07-23 13:43:33 +0800

    Update app_upload.py

--- app_upload.py
+++ app_upload.py
@@ -9,7 +9,7 @@ import sqlalchemy
 import psycopg2
 
 #DATABASE_URL = st.secrets["DATABASE_URL"]
-DATABASE_URL = "postgresql://postgres.idtiedxkkknpmeuaklql:xyspog-wixto5-geMnaf@aws-0-eu-central-1.pooler.supabase.com:5432/postgres"
+
 engine = create_engine(DATABASE_URL)
 
 
